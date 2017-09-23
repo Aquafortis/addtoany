8d6fb346ccbd032aa2a849ef767f96bb1254ceb1
Aquafortis <ian@320up.com.au>
2017-09-23 18:40:00 +1000

    Updated to Version 1.6.2
    See Changelog

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog
+
+## Version 1.6.2
+## 2017-09-23
+- Tidy PHP.
+- Changed JavaScript link.
+- Using Bludit version numbers.
+
+## Version 0.4
+## 2017-07-16
+- Initial release.
--- plugin.php
+++ plugin.php
@@ -4,64 +4,62 @@ class pluginAddToAny extends Plugin {
 
   private $enable;
 
-    private function a2acode()
-      {
-        $ret  = '<!-- AddToAny BEGIN -->
-          <div class="a2a-social" style="margin:20px 0px;">
-            <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-              <a class="a2a_button_facebook"></a>
-              <a class="a2a_button_twitter"></a>
-              <a class="a2a_button_google_plus"></a>
-              <a class="a2a_button_linkedin"></a>
-              <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
-            </div>
-          </div>
-          <script type="text/javascript">
-            var a2a_config = a2a_config || {};
-            a2a_config.icon_color = "unset";';
+    private function a2acode() {
 
-            if ( $this->getDbField('enableMinifyURL') ) {
-              $ret .='a2a_config.track_links = "googl";';
-            };
+    $ret  = '<!-- AddToAny BEGIN -->
+    <div class="a2a-social" style="margin:20px 0px;">
+      <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
+        <a class="a2a_button_facebook"></a>
+        <a class="a2a_button_twitter"></a>
+        <a class="a2a_button_google_plus"></a>
+        <a class="a2a_button_linkedin"></a>
+        <a class="a2a_dd" href="https://www.addtoany.com/share"></a>
+      </div>
+    </div>
+    <script type="text/javascript">
+      var a2a_config = a2a_config || {};
+      a2a_config.icon_color = "unset";';
+      if ( $this->getDbField('enableMinifyURL') ) {
+        $ret .='a2a_config.track_links = "googl";';
+      };
+      $ret .= '</script>
+      <!-- AddToAny END -->';
 
-          $ret .= '</script>
-          <!-- AddToAny END -->';
+      return $ret;
+    }
 
-          return $ret;
-        }
+    public function init() {
+    $this->dbFields = array(
+      'enablePages'=>0,
+      'enablePosts'=>1,
+      'enableDefaultHomePage'=>0,
+      'enableMinifyURL'=>1
+      );
+    }
 
-        public function init()
-        {
-          $this->dbFields = array(
-            'enablePages'=>0,
-            'enablePosts'=>1,
-            'enableDefaultHomePage'=>0,
-            'enableMinifyURL'=>1
-            );
-        }
+    function __construct() {
 
-        function __construct()
-        {
-          parent::__construct();
+      parent::__construct();
 
-          global $Url;
+        global $Url;
 
           $this->enable = false;
 
           if( $this->getDbField('enablePosts') && ($Url->whereAmI()=='post') ) {
             $this->enable = true;
           }
+
           elseif( $this->getDbField('enablePages') && ($Url->whereAmI()=='page') ) {
             $this->enable = true;
           }
-          elseif( $this->getDbField('enableDefaultHomePage') && ($Url->whereAmI()=='home') )
-          {
+
+          elseif( $this->getDbField('enableDefaultHomePage') && ($Url->whereAmI()=='home') ) {
             $this->enable = true;
           }
         }
 
-        public function form()
-        {
+        public function form() {
+
           global $Language;
 
           $html = '<div>';
@@ -91,29 +89,25 @@ class pluginAddToAny extends Plugin {
           return $html;
         }
 
-        public function postEnd()
-        {
+        public function postEnd() {
           if( $this->enable ) {
             return $this->a2acode();
           }
           return false;
         }
 
-        public function pageEnd()
-        {
+        public function pageEnd() {
           if( $this->enable ) {
             return $this->a2acode();
           }
           return false;
         }
 
-        public function siteHead()
-        {
+        public function siteHead() {
           if( $this->enable ) {
-            $html = '<script type="text/javascript" src="//static.addtoany.com/menu/page.js"></script>';
+            $html = '<script async src="https://static.addtoany.com/menu/page.js"></script>';
             return $html;
           }
           return false;
         }
-
 }
